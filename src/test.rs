da8103f12246427df0408af0bc0aa145990bddc2
nan mu <mu.nan.11@outlook.com>
2023-12-29 00:49:54 +0800

    补充了一顿错误报告

--- src/test.rs
+++ src/test.rs
@@ -34,32 +34,33 @@ fn test_2_4() {
     use std::str::from_utf8;
     use std::string::String;
 
-    let conn = Connection::open("res/air_quality.db").expect("Could not open database");
+    let conn = Connection::open("res/air_quality.db")
+        .expect("数据库文件丢失，在./res/下应该有一个air_quality.db");
     let mut stmt = conn//随机取100条数据
         .prepare("select * from air_quality where pm10 is not null and pm25 is not null order by random() limit 100;")
-        .unwrap();
+        .expect("构建查询语句失败");
     let res = stmt
         .query_map([], |row| {
             Ok(Item::new(
-                &(row.get::<usize, String>(1).unwrap()),
-                &(row.get::<usize, String>(2).unwrap()),
-                &(row.get::<usize, String>(3).unwrap()),
-                &(row.get::<usize, String>(4).unwrap()),
+                &(row.get::<usize, String>(1).expect("拆解地区数据失败")),
+                &(row.get::<usize, String>(2).expect("拆解子地区数据失败")),
+                &(row.get::<usize, String>(3).expect("拆解国家数据失败")),
+                &(row.get::<usize, String>(4).expect("拆解城市数据失败")),
                 PmAndYear::Pm10(
-                    row.get::<usize, f64>(5).unwrap(),
+                    row.get::<usize, f64>(5).expect("拆解PM10数据失败"),
                     Local::now()
-                        .with_year(row.get::<usize, i32>(6).unwrap())
-                        .unwrap(),
+                        .with_year(row.get::<usize, i32>(6).expect("拆解PM10年份数据失败"))
+                        .expect("解析PM10年份数据失败"),
                 ),
                 PmAndYear::Pm10(
-                    row.get::<usize, f64>(7).unwrap(),
+                    row.get::<usize, f64>(7).expect("拆解PM2.5数据失败"),
                     Local::now()
-                        .with_year(row.get::<usize, i32>(8).unwrap())
+                        .with_year(row.get::<usize, i32>(8).expect("拆解PM2.5年份数据失败)"))
                         .unwrap(),
                 ),
             ))
         })
-        .unwrap();
+        .expect("迭代数据库回显失败");
     println!("数据准备完毕");
     let res: String = res
         .map(|s| s.unwrap().to_string())
@@ -72,14 +73,15 @@ fn test_2_4() {
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
-        .unwrap();
-    let mut stdin = program.stdin.take().expect("Failed to open stdin");
+        .expect("启动c程序失败");
+    let mut stdin = program.stdin.take().expect("打开程序stdin失败");
     let input = std::thread::spawn(move || {
-        stdin
-            .write_all(res.as_bytes())
-            .expect("Failed to write to stdin");
+        stdin.write_all(res.as_bytes()).expect("标准输入失败");
     });
     input.join().unwrap();
-    let output = program.wait_with_output().unwrap();
-    println!("{}", from_utf8(&output.stdout).unwrap());
+    let output = program.wait_with_output().expect("打开程序stdout失败");
+    println!(
+        "{}",
+        from_utf8(&output.stdout).expect("解析c程序stdout失败")
+    );
 }
